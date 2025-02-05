4cd25c1b24645bf14a5ea98a1eb8fdbd5ccf0b22
Jonas <pankratz.jonas95@gmail.com>
2025-02-05 11:57:38 +0100

    updated

--- refresh.sh
+++ refresh.sh
@@ -5,7 +5,7 @@ cd x86_64
 rm -f ${REPONAME}.db
 rm -f ${REPONAME}.files
 
-repo-add ${REPONAME}.db.tar.gz *.pkg.tar.zst
+repo-add --verify --sign ${REPONAME}.db.tar.gz *.pkg.tar.zst
 
 rm -f ${REPONAME}.db
 rm -f ${REPONAME}.files
